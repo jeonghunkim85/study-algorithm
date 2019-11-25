8efe6e178492386b46559f87a6d803ee0c2358a6
jeonghun.kim <jeonghun.kim@kurlycorp.com>
2019-11-25 09:46:51 +0900

    init project

---
+++ src/test/java/Q20191125Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Q20191125Test {
+
+    Q20191125 solution;
+
+    @BeforeEach
+    void init() {
+        solution = new Q20191125();
+    }
+
+    @Test
+    void test() {
+    }
+}
