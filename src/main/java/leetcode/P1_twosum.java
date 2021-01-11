d6fa680b599cac5c539a91ed7969d1f4b64b7f74
jeonghunkim85 <shockshot@naver.com>
2021-01-12 00:48:44 +0900

    leetcode 1번 문제

---
+++ src/main/java/leetcode/P1_twosum.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P1_twosum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for(var i=0 ; i < nums.length ; i++) {
+            var diff = target - nums[i];
+
+            for(var j=i+1; j < nums.length ; j++) {
+                if(target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap();
+
+        for(int i=0 ; i < nums.length ; i++) {
+            int diff = target - nums[i];
+            if(map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        throw new RuntimeException("there is no solution");
+    }
+}
---
+++ src/test/java/leetcode/P1_twosumTest.java
@@ -0,0 +1,70 @@
+/**
+ * https://leetcode.com/problems/two-sum/
+ *
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ *
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ *
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 103
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ */
+
+package leetcode;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+class P1_twosumTest {
+
+    P1_twosum towsum = new P1_twosum();
+
+    @ParameterizedTest
+    @MethodSource("getTestCases")
+    public void test(int[] nums, int target, int[] expect) {
+        var result = towsum.twoSum(nums, target);
+        assertThat(result[0], is(expect[0]));
+        assertThat(result[1], is(expect[1]));
+    }
+
+    public static Stream<Arguments> getTestCases() {
+        return Stream.of(
+            of(new int[]{2,7,11,15}, 9, new int[]{0,1}),
+            of(new int[]{3,2,4}, 6, new int[]{1,2}),
+            of(new int[]{3,3}, 6, new int[]{0,1})
+        );
+    }
+
+}
